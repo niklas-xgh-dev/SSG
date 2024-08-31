e95d5f420bc13305a21dce922276c69bd18502a8
niklas-xgh-dev <techstag@outlook.com>
2024-08-31 21:56:44 +0200

    todo textNode

---
+++ src/textnode.py
@@ -0,0 +1,5 @@
+#TODO
+#n textnode.py create a class called TextNode. It should have 3 properties that can be set in the constructor:
+#self.text - The text content of the node
+#self.text_type - The type of text this node contains, which is just a string like "bold" or "italic"
+#self.url - The URL of the link or image, if the text is a link. Default to None if nothing is passed in.
