a872503f8547bd90d05b23176781f206f06691b9
niklas-xgh-dev <techstag@outlook.com>
2024-09-05 23:31:59 +0200

    implemented htmlnode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,19 +1,29 @@
 class HTMLNode:
-    def __init__(self, tag, value, children, props) -> None:
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
-        pass
 
     def to_html(self):
         raise NotImplementedError
     
     def props_to_html(self):
-        props_html = f"
-        {
-        'href': '{self.props}', 
-        'target': '_blank',
-        }
-        "
-        return
+        if not self.props:
+            return ""
+        
+        props_html = " ".join(f'{key}="{value}"' for key, value in self.props.items())
+        return f" {props_html}"
+    
+    def __repr__(self):
+        attributes = []
+        if self.tag is not None:
+            attributes.append(f"Tag:{self.tag}")
+        if self.value is not None:
+            attributes.append(f"value:{self.value}")
+        if self.children is not None:
+            attributes.append(f"children:{self.children}")
+        if self.props is not None:
+            attributes.append(f"props:{self.props}")
+        
+        return " ".join(attributes)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from textnode import TextNode
+from htmlnode import HTMLNode
 
 def main():
     obj = TextNode("Hello", "Arial", "www.google.com")
+    htmlobj = HTMLNode()
+    print(HTMLNode)
     print(obj)
 
 if __name__ == "__main__":
